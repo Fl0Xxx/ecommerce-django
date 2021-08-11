461570cede6008f156cbab315db6ec56ae716c7c
Fl0Xxx <82512540+Fl0Xxx@users.noreply.github.com>
2021-08-11 22:22:07 +0300

    final commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'shop.context_processors.menu_links',
+                'shop.context_processors.counter',
             ],
         },
     },
@@ -137,3 +140,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
+from .models import Category
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('product/', views.product, name='product'),
+    path('category/<slug:category_slug>', views.home, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
+    path('cart', views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
+    path('cart/remove/<int:product_id>', views.cart_remove, name='cart_remove'),
+    path('cart/delete/<int:product_id>', views.cart_delete, name='cart_delete'),
+    path('account/create/', views.sign_up_view, name='signup'),
+    path('account/login/', views.login_view, name='login'),
+    path('account/logout/', views.logout_view, name='logout'),
 ]
